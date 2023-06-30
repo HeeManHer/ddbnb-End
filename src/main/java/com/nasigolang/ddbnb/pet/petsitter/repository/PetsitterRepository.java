3c5886628c79fbf99667b226d1b798f0ba6274a4
LJSUNG <ljsung6@naver.com>
2023-06-30 14:47:51 +0900

    feature/petsitter/paging&search

--- src/main/java/com/nasigolang/ddbnb/pet/petsitter/repository/PetsitterRepository.java
+++ src/main/java/com/nasigolang/ddbnb/pet/petsitter/repository/PetsitterRepository.java
@@ -2,19 +2,13 @@ package com.nasigolang.ddbnb.pet.petsitter.repository;
 
 import com.nasigolang.ddbnb.member.entity.Member;
 import com.nasigolang.ddbnb.pet.petsitter.entity.PetsitterEntity;
-import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.util.Optional;
 
 public interface PetsitterRepository extends JpaRepository<PetsitterEntity, Long> {
-
-
-
-    Page <PetsitterEntity> findByLocationContainingAndPetSizeContainingAndCareContainingAndStartDateContainingAndEndDateContainingAndSitterStatus(Pageable page, String location, String petSize, String care, LocalDate startDate, LocalDate endDate, String sitterStatus);
 
     Page<PetsitterEntity> findByMember(Pageable page, Optional<Member> memberId);
 }
